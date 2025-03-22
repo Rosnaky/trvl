6fa730f01d417d6cc99b8abb27219fec49794874
Kiersten E <152218847+kiengen@users.noreply.github.com>
2025-03-22 14:25:19 -0400

    website landing page wip

--- website/src/app/page.js
+++ website/src/app/page.js
@@ -2,8 +2,15 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-screen h-screen bg-background">
-      
+    <div className="w-1/1 h-1/2 bg-foreground">
+       <div className="flex flex-col h-3/4 w-3/4 min-w-300 min-h-500">
+        <div className="flex bg-tertiary">
+          <p>test4444444444</p>
+        </div>
+        <div className="flex bg-foreground">
+          <p>test2</p>
+        </div>
+      </div> 
     </div>
   );
 }
