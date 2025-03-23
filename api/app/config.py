7a759cfc9358a5001c44987dfb5b4d8c45137940
ctluk <ctluk@uwaterloo.ca>
2025-03-23 05:28:27 -0400

    Returns correct itinerary :)
    Co-authored-by: Ronak <Rosnaky@users.noreply.github.com>

--- api/app/config.py
+++ api/app/config.py
@@ -1,4 +1,7 @@
 import os
+import dotenv
+
+dotenv.load_dotenv()
 
 POSTGRES_USER = os.environ.get("POSTGRES_USER")
 POSTGRES_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
@@ -6,8 +9,9 @@ POSTGRES_DB = os.environ.get("POSTGRES_DB")
 POSTGRES_PORT = os.environ.get("POSTGRES_PORT")
 
 class Config:
+    print("IN_DOCKER", os.environ.get("IN_DOCKER"))
     if os.environ.get("IN_DOCKER") == "TRUE":
-        SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@0.0.0.0:{POSTGRES_PORT}/{POSTGRES_DB}"
+        SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@postgres:{POSTGRES_PORT}/{POSTGRES_DB}"
     else:
         SQLALCHEMY_DATABASE_URI = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@0.0.0.0:{POSTGRES_PORT}/{POSTGRES_DB}"
     
